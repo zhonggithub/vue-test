155986bd447ad2ef5554aa012284911c230c5996
hedgehog <465016678@qq.com>
2017-02-21 20:51:27 +0800

    添加sidebar的html，js逻辑还没写

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Hedgehog
+ * @Date: 2017-02-16 19:33:11
+ * @Last Modified by:   hedgehog
+ * @Last Modified time: 2017-02-20 15:54:17
+ */
+
+'use strict';
+
+import VSidebar from './VSidebar';
+
+export default VSidebar;
