d61757d8e991e4d34f9487635cf1ebc152f34e57
hedgehog <465016678@qq.com>
2017-02-21 20:51:14 +0800

    添加sidebar的html，js逻辑还没写

--- src/components/Layout1.jsx
+++ src/components/Layout1.jsx
@@ -1,10 +1,19 @@
+import '../assets/global/plugins/bootstrap/css/bootstrap.css';
+// import '../assets/global/css/components.css';
+// import '../assets/global/css/plugins.css';
+import '../assets/layout/css/layout.css';
+import 'font-awesome/css/font-awesome.min';
+import 'simple-line-icons/css/simple-line-icons';
+// import '../assets/global/plugins/jquery-1.11.0.min.js';
+
 import "./style.less";
 import VIcon from './icon';
 import Navigation from './Navigation';
+import VSidebar from './sidebar';
 
 export default {
   props: {
-    collapse : {
+    collapse: {
       type: Boolean,
       default: false,
     }
@@ -31,16 +40,17 @@ export default {
       <div class={collapse ? "ant-layout-aside ant-layout-aside-collapse" : "ant-layout-aside"}>
         <aside class="ant-layout-sider">
           <div class="ant-layout-logo">
-            {collapse ? <img style={{marginLeft: '-28px'}} src={logo}/> : <img src={logo}/>}
+            {collapse ? <img style={{ marginLeft: '-28px' }} src={logo} /> : <img src={logo} />}
           </div>
           {/*{this.renderSideNav()}*/}
+          <VSidebar />
           <div class="ant-aside-action" onClick={this.onCollapseChange}>
             {collapse ? <VIcon type='right' /> : <VIcon type='left' />}
           </div>
         </aside>
         <div class="ant-layout-main">
           <div class="hms-layout-nav-header">
-            <Navigation/>
+            <Navigation />
           </div>
           <router-view></router-view>
         </div>
